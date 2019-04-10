6c58a3b7a8d0df1b340cb4ecf637f38f741f9d27
Manjit <manjit.kaur16@stjohns.edu>
2019-04-09 20:58:12 -0400

    Init commit

---
+++ PycharmProjects/DancePresentation/flask-dance-twitter/twitter.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
+app.config["TWITTER_OAUTH_CLIENT_KEY"] = os.environ.get("TWITTER_OAUTH_CLIENT_KEY")
+app.config["TWITTER_OAUTH_CLIENT_SECRET"] = os.environ.get("TWITTER_OAUTH_CLIENT_SECRET")
+twitter_bp = make_twitter_blueprint()
+app.register_blueprint(twitter_bp, url_prefix="/login")
+
+@app.route("/")
+def index():
+    if not twitter.authorized:
+        return redirect(url_for("twitter.login"))
+    resp = twitter.get("account/verify_credentials.json")
+    assert resp.ok
+    return "You are @{screen_name} on Twitter".format(screen_name=resp.json()["screen_name"])
